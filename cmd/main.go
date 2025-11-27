0cdb9a2204a00ec15fc6224b3d039e6fb5ee853b
William Dias Marcos <william.diasmarcos@gmail.com>
2025-11-27 07:58:56 -0300

    feat: load variables from .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/William-Dias-Marcos/Anime-API-Golang/controller"
 	"github.com/William-Dias-Marcos/Anime-API-Golang/db"
 	"github.com/William-Dias-Marcos/Anime-API-Golang/repository"
 	"github.com/William-Dias-Marcos/Anime-API-Golang/usecase"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env: %v", err)
+    }
+	
 	server := gin.Default()
+	ginMode := os.Getenv("GIN_MODE")
+	gin.SetMode(ginMode)
 
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
@@ -35,5 +48,6 @@ func main(){
 	server.POST("/animes", AnimeController.CreateAnime)
 	server.DELETE("/animes/:guid", AnimeController.DeleteAnime)
 
-	server.Run(":8000")
+	port := os.Getenv("PORT")
+	server.Run(fmt.Sprintf(":%s", port))
 }
--- db/conn.go
+++ db/conn.go
@@ -3,21 +3,23 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host = "localhost"
-	port = 5432
-	user = "dev"
-	password = "12345678ab"
-	dbname = "animes"
-)
 
 func ConnectDB() (*sql.DB, error) {
-	psqlInfo :=  "host=%s port=%d user=%s " + "password=%s dbname=%s sslmode=disable"
-	psqlInfo =  fmt.Sprintf(psqlInfo, host, port, user, password, dbname)
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
